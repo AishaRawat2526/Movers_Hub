2209a8b8c6e2e0b1a99e8d47ed60506a996a176d
Aisha Rawat <118443278+AishaRawat2526@users.noreply.github.com>
2023-09-04 01:12:37 +0530

    Add files via upload
    commit

---
+++ travels/script.js
@@ -0,0 +1,11 @@
+let nav = document.getElementById('menu_bar_wrapper');
+console.log(nav)
+function showBar() {
+    nav.style.left = "0";
+    console.log(showBar)
+}
+function hideBar() {
+    nav.style.left = "-250px"
+    console.log(hideBar)
+
+}
